2b20c46d4af53ae4336822aa2ce4504e65bd0ef4
snehashah0854 <snehashah0854@gmail.com>
2023-04-05 22:04:50 +0530

    html to mongo

---
+++ backend/server.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const bodyparser = require("body-parser");
+const connection = require("./db");
+const { boolean } = require("joi");
+app.use(bodyparser.urlencoded({extended: true}));
+
+// database connection
+connection();
+
+const notesSchema = {
+    email: { type: String, required: true },
+	verified: {type:Boolean,default:false}
+}
+
+
+const User = mongoose.model("Users",notesSchema);
+console.log(__dirname);
+app.get("/",function(req,res){
+    console.log("in get");
+    res.sendFile(__dirname+"/../frontend/index.html");
+})
+
+//app.post
+
+app.post("/",function(req,res){
+    let newNote = new User({
+        email: req.body.email,
+        verfied: false
+    })
+    newNote.save();
+    console.log("in post ");
+    res.redirect("/");
+})
+
+app.listen(5000, ()=>{
+    console.log("Server on 5000");
+})
